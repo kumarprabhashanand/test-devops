53bebe259ca294b72be5ae08688b69296bed823e
Kumar Prabhash Anand <kumarprabhash1995@gmail.com>
2018-06-16 17:52:27 +0530

    test comment for jenkins

--- src/main/java/kpit/poc/EurekaServerApplication.java
+++ src/main/java/kpit/poc/EurekaServerApplication.java
@@ -3,7 +3,7 @@ package kpit.poc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//test comment for jenkins
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
